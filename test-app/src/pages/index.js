9f99c8aaf9bf06bf2718f9ec2ebc4d12c17b4217
dotarsojat69 <tdeath457@gmail.com>
2024-09-25 11:50:36 +0700

    refactor

--- test-app/src/pages/index.js
+++ test-app/src/pages/index.js
@@ -1,56 +1,9 @@
-import React, { useState, useCallback } from 'react';
+import GenerateAngka from "@/components/GenerateAngka";
 
-const GenerateAngka = () => {
-  const [count, setCount] = useState('');
-  const [numbers, setNumbers] = useState([]);
-
-  const generateRandomNumber = useCallback(() => {
-    return Math.floor(Math.random() * 5) + 1;
-  }, []);
-
-  const generateNumbers = () => {
-    const generatedNumbers = Array.from({ length: parseInt(count) }, generateRandomNumber);
-    setNumbers(generatedNumbers);
-  };
-
-  const handleNumberClick = (index) => {
-    setNumbers(prevNumbers => {
-      const newNumbers = [...prevNumbers];
-      newNumbers[index] = generateRandomNumber();
-      return newNumbers;
-    });
-  };
-
+export default function Home() {
   return (
-    <div className="p-4 items-center">
-      <div className="mb-4 items-center justify-center">
-        <input
-          type="number"
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
-          placeholder="Tuliskan jumlah angka"
-          className="border p-2 mr-2"
-        />
-        <button
-          onClick={generateNumbers}
-          className="bg-slate-500 text-white px-4 py-2 rounded hover:bg-slate-800"
-        >
-          Generate
-        </button>
-      </div>
-      <div className="grid grid-cols-4 gap-2">
-        {numbers.map((number, index) => (
-          <div 
-            key={index} 
-            className="border w-16 h-16 flex items-center justify-center text-2xl cursor-pointer hover:bg-gray-100"
-            onClick={() => handleNumberClick(index)}
-          >
-            {number}
-          </div>
-        ))}
-      </div>
+    <div className="flex justify-center items-center h-screen">
+      <GenerateAngka />
     </div>
   );
-};
-
-export default GenerateAngka;
+}
